fe9638842319c6cf34965474d6b494c4e58ef95a
Maxime Danic <maxime.danic@gmail.com>
2024-03-05 23:31:28 +0200

    Add CardPhoto component to display media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,7 @@
 import getPhotographe from '../utils/getPhotographe.js';
 import createPhotographerHeader from '../components/photographerHeader.js';
 import getMediaFromPhotographer from '../utils/getMediaFromPhotographe.js';
+import createCardPhoto from '../components/CardPhoto.js';
 
 // get the id from the url
 const params = new URLSearchParams(window.location.search);
@@ -12,9 +13,18 @@ const photographerDetail = await getPhotographe(idPhotographe);
 // get the media from the photographer
 const media = await getMediaFromPhotographer(idPhotographe);
 console.log(media);
+
 // create the header
 const photographerHeader = createPhotographerHeader(photographerDetail);
 
 // display the data
 const photographerContainer = document.querySelector('.photograph-header');
 photographerContainer.innerHTML = photographerHeader;
+
+// display the media
+const mediaContainer = document.querySelector('.photograph_media');
+
+media.map((item) => {
+  const cardPhoto = createCardPhoto(item);
+  mediaContainer.innerHTML += cardPhoto;
+});

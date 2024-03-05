15913451709e553d74cc75d9bc2b68b2c999402a
Maxime Danic <maxime.danic@gmail.com>
2024-03-05 21:23:40 +0200

    Add getMediaFromPhotographer function to retrieve media for a photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,5 +1,6 @@
 import getPhotographe from '../utils/getPhotographe.js';
 import createPhotographerHeader from '../components/photographerHeader.js';
+import getMediaFromPhotographer from '../utils/getMediaFromPhotographe.js';
 
 // get the id from the url
 const params = new URLSearchParams(window.location.search);
@@ -8,6 +9,9 @@ const idPhotographe = params.get('id');
 // get the data from the photographer
 const photographerDetail = await getPhotographe(idPhotographe);
 
+// get the media from the photographer
+const media = await getMediaFromPhotographer(idPhotographe);
+console.log(media);
 // create the header
 const photographerHeader = createPhotographerHeader(photographerDetail);
 

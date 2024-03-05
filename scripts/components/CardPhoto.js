acff76dfb814c4bdd7afced10cd72da286dae43a
Maxime Danic <maxime.danic@gmail.com>
2024-03-06 01:44:42 +0200

    Refactor CardPhoto component to support both images and videos

--- scripts/components/CardPhoto.js
+++ scripts/components/CardPhoto.js
@@ -1,22 +1,32 @@
 import heartSvg from '../../assets/icons/heart.js';
+import MediaFactory from '../utils/MediaFactory.js';
 
 export default function createCardPhoto({
   image,
+  video,
   likes,
   title,
+  id: idMedia,
   photographerId,
 }) {
-  const picture = `assets/Sample Photos/${photographerId}/${image}`;
-  const heart = heartSvg();
+  const typeSource = video ? 'video' : 'image';
+  const mediaFile = image || video;
+  const pictureSource = `assets/Sample Photos/${photographerId}/${mediaFile}`;
+  const mediaObject = new MediaFactory(typeSource, pictureSource);
+  const displayMedia = mediaObject.createMedia().display();
+
+  const heartIcon = heartSvg();
 
   return `
     <div class="card">
-        <img src="${picture}" alt="${title}" class="card__photo">
+    <a href="${idMedia}" aria-label="${title}">
+      ${displayMedia}
+      </a>
         <div class="card__info">
           <p class="card__title">${title}</p>
           <div class="card__likes">
             <p class="card__likes-count">${likes}</p>
-            ${heart}
+            ${heartIcon}
           </div>
       `;
 }

1fa7da4da7064fc8930a794f2314630653a846cc
Maxime Danic <maxime.danic@gmail.com>
2024-03-05 21:06:37 +0200

    Add photographer detail and header to photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,16 @@
-// Mettre le code JavaScript lié à la page photographer.html
+import getPhotographe from '../utils/getPhotographe.js';
+import createPhotographerHeader from '../components/photographerHeader.js';
+
+// get the id from the url
+const params = new URLSearchParams(window.location.search);
+const idPhotographe = params.get('id');
+
+// get the data from the photographer
+const photographerDetail = await getPhotographe(idPhotographe);
+
+// create the header
+const photographerHeader = createPhotographerHeader(photographerDetail);
+
+// display the data
+const photographerContainer = document.querySelector('.photograph-header');
+photographerContainer.innerHTML = photographerHeader;

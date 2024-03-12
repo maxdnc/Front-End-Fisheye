4c5bcea4a1e6a35ccbf854f8425f59ba6c44b2ce
Maxime Danic <maxime.danic@gmail.com>
2024-03-12 02:17:11 +0200

    Refactor LightBox.js to improve code structure and add functionality

--- scripts/components/LightBox.js
+++ scripts/components/LightBox.js
@@ -1,26 +1,67 @@
 import MediaFactory from '../utils/MediaFactory.js';
 
-const currentMedia = document.querySelector('.current-media');
+const lightboxMediaContainer = document.querySelector('.current-media');
 
-async function createLightBox({ image, video, title, photographerId }) {
-  const typeSource = video ? 'video' : 'image';
-  const mediaFile = image || video;
-  const pictureSource = `assets/Sample Photos/${photographerId}/${mediaFile}`;
+// Function to create and display media in the lightbox
+async function displayMediaInLightbox({ image, video, title, photographerId }) {
+  const mediaType = video ? 'video' : 'image';
+  const mediaFileName = image || video;
+  const mediaFilePath = `assets/Sample Photos/${photographerId}/${mediaFileName}`;
   const mediaObject = new MediaFactory(
-    typeSource,
-    pictureSource,
+    mediaType,
+    mediaFilePath,
     title,
     'lightbox__media'
   );
-  const displayMedia = mediaObject.createMedia().display();
-  currentMedia.innerHTML = displayMedia;
+  const mediaHTML = mediaObject.createMedia().display();
+  lightboxMediaContainer.innerHTML = mediaHTML;
 }
 
-export default createLightBox;
-
-const closeLightBox = document.querySelector('.close_lightbox_modal_button');
-
-closeLightBox.addEventListener('click', () => {
+// Close the lightbox
+const lightboxCloseButton = document.querySelector(
+  '.close_lightbox_modal_button'
+);
+lightboxCloseButton.addEventListener('click', () => {
   const lightbox = document.querySelector('#lightbox_modal');
   lightbox.style.display = 'none';
 });
+
+// Function to manage the media in the lightbox
+function handleLightboxMedia(dataFromPhotographer) {
+  let currentMediaIndex = 0;
+  const lightboxLinks = document.querySelectorAll('.link-to-lightBox');
+  lightboxLinks.forEach((link) => {
+    link.addEventListener('click', async (e) => {
+      e.preventDefault();
+      const id = Number(link.id);
+      const clickedMedia = dataFromPhotographer.find((item) => item.id === id);
+      currentMediaIndex = dataFromPhotographer.indexOf(clickedMedia);
+      displayMediaInLightbox(clickedMedia);
+    });
+  });
+
+  const nextMediaButton = document.querySelector('.lightbox-button-next');
+  const previousMediaButton = document.querySelector(
+    '.lightbox-button-previous'
+  );
+
+  nextMediaButton.addEventListener('click', () => {
+    currentMediaIndex += 1;
+    if (currentMediaIndex >= dataFromPhotographer.length) {
+      currentMediaIndex = 0; // reset the index
+    }
+    const nextMedia = dataFromPhotographer[currentMediaIndex];
+    displayMediaInLightbox(nextMedia);
+  });
+
+  previousMediaButton.addEventListener('click', () => {
+    currentMediaIndex -= 1;
+    if (currentMediaIndex < 0) {
+      currentMediaIndex = dataFromPhotographer.length - 1; // reset the index
+    }
+    const previousMedia = dataFromPhotographer[currentMediaIndex];
+    displayMediaInLightbox(previousMedia);
+  });
+}
+
+export default handleLightboxMedia;

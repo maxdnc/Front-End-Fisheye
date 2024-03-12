6a21db4142c3c00fac20681679fa09bf081189e6
Maxime Danic <maxime.danic@gmail.com>
2024-03-12 02:08:20 +0200

    Add lightbox functionality to photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,13 +3,14 @@ import createHeader from '../components/photographerHeader.js';
 import getMediaFromPhotographer from '../utils/getMediaFromPhotographe.js';
 import createCardPhoto from '../components/CardPhoto.js';
 import onSubmit from '../utils/getContactFormValue.js';
-
+import createLightBox from '../components/LightBox.js';
 // get the id from the url
 const params = new URLSearchParams(window.location.search);
 const idPhotographe = params.get('id');
 
 // get the data from the photographer
 const photographerDetail = await getPhotographe(idPhotographe);
+console.log(photographerDetail);
 
 // get the media from the photographer
 const mediaFromPhotographer = await getMediaFromPhotographer(idPhotographe);
@@ -30,3 +31,38 @@ mediaFromPhotographer.map((item) => {
 // get data form the form
 const contactForm = document.querySelector('#contact-modal-form');
 contactForm.addEventListener('submit', onSubmit);
+
+// lightbox
+let currentIndex = 0;
+const linkTolightBox = document.querySelectorAll('.link-to-lightBox');
+linkTolightBox.forEach((link) => {
+  link.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const id = Number(link.id);
+    const media = mediaFromPhotographer.find((item) => item.id === id);
+    currentIndex = mediaFromPhotographer.indexOf(media);
+    createLightBox(media);
+  });
+  // next
+});
+
+const nextButton = document.querySelector('.lightbox-button-next');
+const previousButton = document.querySelector('.lightbox-button-previous');
+
+nextButton.addEventListener('click', () => {
+  currentIndex += 1;
+  if (currentIndex >= mediaFromPhotographer.length) {
+    currentIndex = 0; // reset the index
+  }
+  const nextMedia = mediaFromPhotographer[currentIndex];
+  createLightBox(nextMedia);
+});
+
+previousButton.addEventListener('click', () => {
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = mediaFromPhotographer.length - 1; // reset the index
+  }
+  const previousMedia = mediaFromPhotographer[currentIndex];
+  createLightBox(previousMedia);
+});

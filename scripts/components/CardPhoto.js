0a3a394badc08ccbf99cc21a72f63b6c1421d20f
Maxime Danic <maxime.danic@gmail.com>
2024-03-12 12:46:17 +0200

    Update link in CardPhoto.js to be focusable

--- scripts/components/CardPhoto.js
+++ scripts/components/CardPhoto.js
@@ -24,7 +24,7 @@ export default function createCardPhoto({
 
   return `
     <div class="card">
-    <a id="${idMedia}" class="link-to-lightBox">
+    <a id="${idMedia}" class="link-to-lightBox" href="#">
       ${displayMedia}
       </a>
         <div class="card__info">

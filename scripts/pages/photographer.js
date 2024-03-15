f7913b00a242139c836fc35d64079b5a1e1ca5d6
Maxime Danic <maxime.danic@gmail.com>
2024-03-15 10:47:33 +0200

    Add media filtering functionality

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -9,23 +9,70 @@ import {
   displayDayPrice,
   manageLikeCard,
 } from '../components/LikeCounter.js';
+import {
+  filterMedia,
+  handleOptionClick,
+  sortMedia,
+} from '../components/FilterMedia.js';
+
 // get the id from the url
 const params = new URLSearchParams(window.location.search);
 const idPhotographe = params.get('id');
 
 // get the data from the photographer
 const photographerDetail = await getPhotographe(idPhotographe);
+// display the media
+const mediaContainer = document.querySelector('.photograph_media');
 
 // get the media from the photographer
 const mediaFromPhotographer = await getMediaFromPhotographer(idPhotographe);
+let currentMedia = mediaFromPhotographer;
+
+// filter media
+filterMedia();
+
+const button = document.querySelector('#media-filter-button');
+const dropdown = document.querySelector('#media-filter-options');
+const options = dropdown.querySelectorAll('button');
+const buttonContent = button.querySelector('#filter-selected');
+const buttonIcon = button.querySelector('.arrow-down');
+
+let selectedOption = null;
+
+// Add an event listener to each option
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    handleOptionClick(
+      option,
+      button,
+      buttonContent,
+      buttonIcon,
+      dropdown,
+      options
+    );
+    selectedOption = option;
+    currentMedia = sortMedia(selectedOption, mediaFromPhotographer);
+    // Clear the current media
+    mediaContainer.innerHTML = '';
+    currentMedia.map((item) => {
+      const cardPhoto = createCardPhoto(item);
+      mediaContainer.innerHTML += cardPhoto;
+      return null;
+    });
+    // Call manageLikeCard after the media cards are added to the DOM
+    const likeButtons = document.querySelectorAll('.card__likes-button');
+    manageLikeCard(likeButtons);
+
+    handleLightboxMedia(currentMedia);
+
+    // Add the sorted media to the container
+  });
+});
 
 // create the header
 createHeader(photographerDetail);
 
-// display the media
-const mediaContainer = document.querySelector('.photograph_media');
-
-mediaFromPhotographer.map((item) => {
+currentMedia.map((item) => {
   const cardPhoto = createCardPhoto(item);
   mediaContainer.innerHTML += cardPhoto;
   return null;
@@ -36,10 +83,10 @@ const contactForm = document.querySelector('#contact-modal-form');
 contactForm.addEventListener('submit', onSubmit);
 
 // lightbox
-handleLightboxMedia(mediaFromPhotographer);
+handleLightboxMedia(currentMedia);
 
 // counter likes box
-displayLikesBoxContent(mediaFromPhotographer);
+displayLikesBoxContent(currentMedia);
 displayDayPrice(photographerDetail);
 
 // card toggle like button

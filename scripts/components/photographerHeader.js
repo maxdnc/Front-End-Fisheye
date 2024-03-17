fec6c9bf1399bfde02e27a957167b41ddd1e2198
Maxime Danic <maxime.danic@gmail.com>
2024-03-17 12:23:18 +0200

    Update path for photographer's ID photos

--- scripts/components/photographerHeader.js
+++ scripts/components/photographerHeader.js
@@ -5,7 +5,7 @@ async function createPhotographerHeader({
   country,
   tagline,
 }) {
-  const picture = `assets/Sample Photos/Photographers ID Photos/${portrait}`;
+  const picture = `assets/Sample Photos/Photographers_ID_Photos/${portrait}`;
   return `
       <div>
         <h1 class="photographer__name">${name}</h1>

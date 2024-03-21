d0d5ff27d47ba265f7b010abd1a3315d7c76cd15
Maxime Danic <maxime.danic@gmail.com>
2024-03-21 22:18:02 +0200

    Update alt attribute in photographer template

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,7 +5,7 @@ export default function photographerTemplate(data) {
 
   const template = `
       <a href="photographer.html?id=${id}" class="link" aria-label='${name}'>
-      <img src="${picture}" alt="">
+      <img src="${picture}" alt="${name}">
       <h2>${name}</h2> </a>
       <div class="description">
         <p>${city}, ${country}</p>
